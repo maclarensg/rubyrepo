a39afcf5dfd5ddb94fc7ffd642afdf69ac64fe24
Gavin <maclarensg>
2019-12-01 14:23:42 +0800

    save

---
+++ quiz/3way_min_sum_part/QNS.md
@@ -0,0 +1,3 @@
+Given n, find x, y and z such that x y z = n and x + y + z is minimal; for instance, given n = 1890, the correct answer is (9 14 15).
+
+Your task is to write a program to find x, y and z. When you are finished, you are welcome to read or run a suggested solution, or to post your own solution or discuss the exercise in the comments below.
---
+++ quiz/3way_min_sum_part/run.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+
+def solve(n)
+   
+end
+
+
+def get_divisors(n)
+    factors = [1]
+    for i in 2..n 
+        factors.append i if n % i == 0
+    end
+    return factors
+end
+
+def prime(n)
+    return false if n < 2  
+    (2..n/2).none?{|i| n % i == 0}
+end
+
+def min_sum_factorize(n, num_factors)
+    if num_factors == 1
+        return [n]
+    end
+    
+    best = [n] + [1] * (num_factors - 1)
+    best_sum = best.sum
+
+    get_divisors(n).each do |x|
+        if x ** num_factors > n
+            next
+        end
+        if n % x == 0
+            rest = min_sum_factorize( n/x, num_factors - 1)
+            total = rest.sum + x
+            if total < best_sum
+                best = [x] + rest
+                best_sum = total
+            end
+        end
+    end
+    return best
+end
+
+p min_sum_factorize(1089, 2)
